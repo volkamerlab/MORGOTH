4fdf1452305b51fa837efc615ddebf650252ca33
Lisa-Marie Rolli <lisa-marie.rolli@bioinf.uni-sb.de>
2025-05-30 17:36:45 +0200

    added a version

--- morgoth/__init__.py
+++ morgoth/__init__.py
@@ -2,3 +2,5 @@ from morgoth.morgoth import *
 from morgoth.multivariate_dt import *
 from morgoth.CP_Pipeline.conformal_prediction import *
 from morgoth.similarity_test_train import *
+from morgoth.multi_rf_main import *
+__version__ = 1.1
