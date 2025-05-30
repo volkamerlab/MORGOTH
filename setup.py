1282f623289c762e3171541d889885d758d4d3ca
Lisa-Marie Rolli <lisa-marie.rolli@uni-saarland.de>
2025-05-30 12:13:16 +0200

    setting up package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='morgoth',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Lisa-Marie Rolli',
+    author_email='lisa-marie.rolli@uni-saarland.de',
+    description='Interpretable and reliable multivariate random forest for simultaneous classification and regression',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/volkamerlab/MORGOTH.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
